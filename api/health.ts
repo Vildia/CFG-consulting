c3d2c72829b6dcfc0502e65b4eb5e9f94caab0da
Евгений <97235169+Vildia@users.noreply.github.com>
2025-09-18 21:56:33 +0800

    Delete api directory

--- api/health.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(req:VercelRequest, res:VercelResponse){
-  const hasURL = !!process.env.APPS_SCRIPT_URL;
-  const hasKey = !!process.env.CFG_KEY;
-  const origin = String(req.headers.origin || ('https://' + (req.headers.host || '')));
-  res.status(200).json({ ok:true, env:{ has_APPS_SCRIPT_URL:hasURL, has_CFG_KEY:hasKey, origin, ORIGIN:(process.env.ORIGIN||''), ORIGIN_PREVIEW:(process.env.ORIGIN_PREVIEW||'') } });
-}
