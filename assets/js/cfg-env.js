d835bd92a7aab8dcacb668eb8f9659bdadbd21ca
Евгений <97235169+Vildia@users.noreply.github.com>
2025-09-22 23:33:34 +0800

    Delete assets directory

--- assets/js/cfg-env.js
+++
@@ -1,7 +0,0 @@
-// Runtime config; values can be overridden by Vercel injecting window.__CFG
-window.__CFG = Object.assign({
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwFOxaAtkPV4hsk3_gHGdrS4dlISHyVtj2f8TrxjTI_ZQP7j8cd2N8XVUdYpPzUNPv73A/exec',
-  GA_ID: 'G-D7QRTPW2F3',
-  CFG_KEY: '',
-  CFG_SECRET: ''
-}, window.__CFG||{});
